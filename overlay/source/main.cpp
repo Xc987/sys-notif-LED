e09d5cbeffa253443f9de1fd7a7d7e8063168a7d
Xc987 <ricards.lacis18@gmail.com>
2025-11-16 23:12:14 +0200

    add text showing current mode in overlay

--- overlay/source/main.cpp
+++ overlay/source/main.cpp
@@ -5,36 +5,44 @@
 
 class GuiTest : public tsl::Gui {
 public:
+    std::string modeString = "unknown";
     GuiTest() { }
+    void loadMode() {
+        fsdevMountSdmc();
+        std::ifstream tf("sdmc:/config/sys-notif-LED/type");
+        if (tf.good())
+            std::getline(tf, modeString);
+        else
+            modeString = "(not found)";
+    }
 
     virtual tsl::elm::Element* createUI() override {
+        loadMode();
         auto frame = new tsl::elm::OverlayFrame("sys-notif-LED", "1.0.0");
         auto list = new tsl::elm::List();
 
         auto solidItem = new tsl::elm::ListItem("Set LED to Solid");
-        solidItem->setClickListener([](u64 keys) {
+        solidItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "solid";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "solid";
+                loadMode();
                 return true;
             }
             return false;
         });
         list->addItem(solidItem);
 
+
+        // ===== DIM =====
         auto dimItem = new tsl::elm::ListItem("Set LED to Dim");
-        dimItem->setClickListener([](u64 keys) {
+        dimItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "dim";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "dim";
+                loadMode();
                 return true;
             }
             return false;
@@ -42,14 +50,12 @@ public:
         list->addItem(dimItem);
         
         auto fadeItem = new tsl::elm::ListItem("Set LED to Fade");
-        fadeItem->setClickListener([](u64 keys) {
+        fadeItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "fade";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "fade";
+                loadMode();
                 return true;
             }
             return false;
@@ -57,58 +63,56 @@ public:
         list->addItem(fadeItem);
 
         auto offItem = new tsl::elm::ListItem("Set LED to Off");
-        offItem->setClickListener([](u64 keys) {
+        offItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "off";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "off";
+                loadMode();
                 return true;
             }
             return false;
         });
         list->addItem(offItem);
 
-        auto chargeitem = new tsl::elm::ListItem("Dim when charging");
-        chargeitem->setClickListener([](u64 keys) {
+        auto chargeItem = new tsl::elm::ListItem("Dim when charging");
+        chargeItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "charge";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "charge";
+                loadMode();
                 return true;
             }
             return false;
         });
-        list->addItem(chargeitem);
+        list->addItem(chargeItem);
 
         auto batteryItem = new tsl::elm::ListItem("Dim <=15%, blink <=5% battery");
-        batteryItem->setClickListener([](u64 keys) {
+        batteryItem->setClickListener([this](u64 keys) {
             if (keys & HidNpadButton_A) {
-                const std::string resetPath = "sdmc:/config/sys-notif-LED/reset";
-                const std::string typePath  = "sdmc:/config/sys-notif-LED/type";
                 fsdevMountSdmc();
-                std::ofstream resetFile(resetPath);
-                std::ofstream typeFile(typePath);
-                typeFile << "battery";
+                std::ofstream("sdmc:/config/sys-notif-LED/reset");
+                std::ofstream("sdmc:/config/sys-notif-LED/type") << "battery";
+                loadMode();
                 return true;
             }
             return false;
         });
         list->addItem(batteryItem);
 
+        auto modeDrawer = new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
+            std::string line = "Current mode: " + modeString;
+            renderer->drawString(line.c_str(), false, x + 3, y + 30, 20, renderer->a(0xFFFF));
+        });
+        list->addItem(modeDrawer, 50);
         frame->setContent(list);
         return frame;
     }
 
     virtual void update() override { }
 
-    virtual bool handleInput(u64 keysDown, u64 keysHeld, const HidTouchState &touchPos,
-                             HidAnalogStickState joyStickPosLeft, HidAnalogStickState joyStickPosRight) override {
+    virtual bool handleInput(u64 keysDown, u64 keysHeld, const HidTouchState &touchPos, HidAnalogStickState joyStickPosLeft, HidAnalogStickState joyStickPosRight) override {
         return false;
     }
 };
